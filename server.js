64d466332b283d6187b2ece001fe461219463290
nowscott <nowscott@qq.com>
2024-06-13 01:51:24 +0800

    修复bug

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.post('/api/subscribe', async (req, res) => {  // 更改路径为 /api/subscribe
+app.post('/api/subscribe', async (req, res) => {
     const { name, email } = req.body;
 
     try {
